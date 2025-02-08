f8fddb639feab1f2ea87400e304f484306c8b62a
ericafk0001 <dxslyofficial@gmail.com>
2025-02-08 18:57:15 -0500

    add dash

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -3,23 +3,26 @@ extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_buffer_timer: Timer = $JumpBufferTimer
 @onready var coyote_timer: Timer = $CoyoteTimer
+@onready var dash_timer: Timer = $DashTimer
 
 const SPEED = 140.0
 const JUMP_VELOCITY = -275.0
+const GRAVITY = -960.0
+const DASH_SPEED = 360.0
 
 var is_jumping = false
 var jump_buffered = false
-
 var has_coyote_time = false
+var is_dashing = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if !is_on_floor() && !is_dashing:
+		velocity += calculate_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor() or !coyote_timer.is_stopped():
+		if is_on_floor() && !is_dashing || !coyote_timer.is_stopped() && !is_dashing:
 			is_jumping = true
 			jump_buffered = false
 			velocity.y = JUMP_VELOCITY
@@ -32,6 +35,15 @@ func _physics_process(delta: float) -> void:
 
 	var direction := Input.get_axis("move_left", "move_right")
 	
+	if Input.is_action_just_pressed("dash") && !is_dashing:
+		if direction:
+			is_dashing = true
+			velocity.x = direction * DASH_SPEED
+			velocity.y = 0
+			dash_timer.start()
+		else:
+			is_dashing = false
+	
 	#Flip Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
@@ -39,7 +51,7 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true
 	
 	#animations
-	if is_on_floor():
+	if is_on_floor() && !is_dashing:
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
@@ -47,10 +59,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite.play("jump")
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if is_dashing:
+		animated_sprite.play("dash")
+	
+	if !is_dashing:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	var was_on_floor = is_on_floor()
 	move_and_slide()
@@ -75,3 +91,13 @@ func _on_jump_buffer_timer_timeout() -> void:
 
 func _on_coyote_timer_timeout() -> void:
 	has_coyote_time = false
+
+func calculate_gravity() -> Vector2:
+	if velocity.y < 0:  # Rising
+		return Vector2(0, -GRAVITY)
+	else:  # Falling
+		return Vector2(0, -GRAVITY * 1.7)  # multiply gravity for falling
+
+
+func _on_dash_timer_timeout() -> void:
+	is_dashing = false
